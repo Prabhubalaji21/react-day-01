1823e2dc57e9dc7065dad3a03e11a1e2c050608a
Prabhubalaji21 <prabhubalaji73@gmail.com>
2023-10-30 19:36:06 +0530

    constant values add panni kaati iruku

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@
 //for now it does not receive any arguements
 const App = () => {
   const now = new Date();
+  const a=10;
+  const b=20;
+
+  console.log(now, a+b);
   //it can also execute or contain javascript
   console.log('Hello from component App');
 
@@ -11,6 +15,9 @@ const App = () => {
 return(
   <div>
     <p>Hello world! It is { now.toString() }</p>
+    <p>
+      {a} plus {b} is {a+b};
+    </p>
   </div>
 )
 }
