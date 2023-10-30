ee877c5f7f951e6350182ad41bbe4a9fc22d9c41
Prabhubalaji21 <prabhubalaji73@gmail.com>
2023-10-30 19:54:30 +0530

    Multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 //our first react component
 //javascript function
 //for now it does not receive any arguements
+const Hello = ( ) => {
+  return(
+    <div>
+      <p>Hello everyone...!!!</p>
+    </div>
+  )
+}
 const App = () => {
-  const now = new Date();
-  const a=10;
-  const b=20;
+  // const now = new Date();
+  // const a=10;
+  // const b=20;
 
-  console.log(now, a+b);
-  //it can also execute or contain javascript
-  console.log('Hello from component App');
+  // console.log(now, a+b);
+  // //it can also execute or contain javascript
+  // console.log('Hello from component App');
 
   //returns a html
   //dynamic content ah print pannalam example:- date and time
 return(
   <div>
-    <p>Hello world! It is { now.toString() }</p>
-    <p>
-      {a} plus {b} is {a+b};
-    </p>
+    <h1>Greetings </h1>
+    <Hello />
+    <Hello />
+    <Hello />
+    <Hello />
+    <Hello />
   </div>
 )
 }
